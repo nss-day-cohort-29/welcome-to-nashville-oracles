753fb2ba1488c800787ebad2b2373d8a266f948b
Hannah Neal <hannahmneal@gmail.com>
2018-12-07 15:29:09 -0600

    Added form for API search fields and search buttons

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 console.log("hello main.js")
+
+//              ****     Suggestions for the Group       *****
+
+
+
+
+//              **** Allison     ****
+
+
+
+
+//              **** Colleen    ****
+
+
+
+
+//              **** Grady      ****
+
+
+
+
+//              **** Hannah     ****
+
+
+
