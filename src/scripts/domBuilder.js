7e35b086bfac2822ae82b9a86934487b90c88593
Hannah Neal <hannahmneal@gmail.com>
2018-12-10 14:16:37 -0600

    added form container via JS

--- src/scripts/domBuilder.js
+++ src/scripts/domBuilder.js
@@ -1,32 +1,32 @@
 console.log("hello domBuilder.js")
 
-//Place your section (restaurants, parks, etc.) in domBuilder so it appears as HTML. 
+const domBuilder =  {
 
-//Colleen
+  name: "Dom Builder Component",
 
-let something = function () {
-  let acontainerforourdiv = document.querySelector("#containerSearch");
-  acontainerforourdiv.innerHTML =
-  `<article> 
-  <input type="text" id="foodSearch" placeholder="Restaurants Search">
-  <button type="submit" id="foodBtn">Search Restaurants</button>
-  </article>`
-  console.log("does this work");
+  appendInputForm () {
+    let divFormContainer = document.querySelector("#form-container");
+   divFormContainer.innerHTML =
+    `
+    <article> 
+    <input type="text" id="parksSearch" placeholder="Search Parks">
+    <button type="submit" id="parkSearchBtn">Search Parks</button>
+    </article>
 
-  //Allison
-
-
-
-  //Grady
-
-
-
-  //Hannah
-
-
-
+    <article> 
+    <input type="text" id="foodSearch" placeholder="Restaurants Search">
+    <button type="submit" id="foodBtn">Search Restaurants</button>
+    </article>
+    
+    <article>
+    <input type="text" id="meetSearch" placeholder="Meetups Search">
+    <button type="submit" id="meetBtn">Search Meetups</button>
+    </article>
+  
+    <article>
+    <input type="text" id="musicSearch" placeholder="Concerts Search">
+    <button type="submit" id="musicBtn">Search Concerts</button>
+    </article>`
+  }
 }
-
-something();
-
-
+//call me in main.js!()
