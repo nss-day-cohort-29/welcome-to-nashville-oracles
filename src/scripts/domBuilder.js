a73697d5aab6897f4c501983b24d3b8210a37527
Hannah Neal <hannahmneal@gmail.com>
2018-12-10 15:00:10 -0600

    added display container and minor styling for elements

--- src/scripts/domBuilder.js
+++ src/scripts/domBuilder.js
@@ -26,7 +26,31 @@ const domBuilder =  {
     <article>
     <input type="text" id="musicSearch" placeholder="Concerts Search">
     <button type="submit" id="musicBtn">Search Concerts</button>
-    </article>`
-  }
+    </article>`;
+
+  // ********** DISPLAY *********
+  let divDisplayContainer = document.querySelector("#display-container");
+  divDisplayContainer.innerHTML = `
+    <article id="park-article">
+      <input type="text" name="Parks" id="park-return" placeholder="List of Parks">
+      <button type="submit" id="park-save">Save</button>
+    </article>
+
+    <article id="food-article">
+      <input type="text" name="Cuisines" id="food-return" placeholder="List of Restaurants">
+      <button type="submit" id="food-save">Save</button>
+    </article>
+
+    <article id="meet-article">
+      <input type="text" name="Meetups" id="meet-return" placeholder="List of Meetups">
+      <button type="submit" id="meet-save">Save</button>
+      </article>
+
+      <input type="text" name="Concerts" id="music-return" placeholder="List of concerts">
+      <button type="submit" id="music-save">Save</button>
+    </article>`;
+  },
 }
 //call me in main.js!()
+
+//Notes: Check input type="text"?? for results list
