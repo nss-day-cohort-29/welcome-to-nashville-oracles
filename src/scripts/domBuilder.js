cc2cb727dd725e9a26e3239f0e4e16452e4f0df3
Colleen Woolsey <rlwool@aol.com>
2018-12-11 17:36:28 -0600

    ...

--- src/scripts/domBuilder.js
+++ src/scripts/domBuilder.js
@@ -14,8 +14,8 @@ const domBuilder =  {
     </article>
 
     <article id="cuisine-article">
-    <input type="text" name="Cuisines" id="cuisine-input" placeholder="Search by Cuisine">
-    <button type="submit" id="cuisine-search">Search RESTAURANTS</button>
+    <input type="text" name="Cuisines" id="cuisine-input" placeholder="restaurants by food type">
+    <button type="submit" id="cuisine-search">Search restauarants by food type</button>
     </article>
     
     <article>
@@ -26,28 +26,30 @@ const domBuilder =  {
     <article>
     <input type="text" id="musicSearch" placeholder="Concerts Search">
     <button type="submit" id="musicBtn">Search Concerts</button>
-    </article>`
+    </article>`;
 
   // ********** DISPLAY *********
   let divDisplayContainer = document.querySelector("#display-container");
   divDisplayContainer.innerHTML = `
   <article id="park-article">
-    <input type="text" name="Parks" id="park-return" placeholder="List of Parks">
-    <button type="submit" id="park-save">Save</button>
-  </article>
-  <article id="food-article">
-    <input type="text" name="Restaurants" id="food-return" placeholder="List of Restaurants">
-    <button type="submit" id="food-save">Save</button>
-  </article>
-  <article id="meet-article">
-    <input type="text" name="Meetups" id="meet-return" placeholder="List of Meetups">
-    <button type="submit" id="meet-save">Save</button>
-  </article>
-  <article id = "music-article">
-    <p>type="text" name="Concerts" id="music-return" placeholder="List of concerts"</p>
-    <button type="submit" id="music-save">Save</button>
-  </article>`
-  },
+      <input type="text" name="Parks" id="park-return" placeholder="List of Parks">
+      <button type="submit" id="park-save">Save</button>
+    </article>
+
+    <article id="food-article">
+      <input type="text" name="Cuisines" id="food-return" placeholder="List of Restaurants">
+      <button type="submit" id="food-save">Save</button>
+    </article>
+
+    <article id="meet-article">
+      <input type="text" name="Meetups" id="meet-return" placeholder="List of Meetups">
+      <button type="submit" id="meet-save">Save</button>
+      </article>
+
+      <input type="text" name="Concerts" id="music-return" placeholder="List of concerts">
+      <button type="submit" id="music-save">Save</button>
+    </article>`;
+  }  
 }
 //call me in main.js!()
 
--- src/scripts/eventListener.js
+++ src/scripts/eventListener.js
@@ -6,77 +6,8 @@ cuisinesArray = [
             "cuisine_id": 152,
             "cuisine_name": "African"
           }
-        },
-        {
-          "cuisine": {
-            "cuisine_id": 1,
-            "cuisine_name": "American"
-          }
-        },
-        {
-          "cuisine": {
-            "cuisine_id": 3,
-            "cuisine_name": "Asian"
-          }
-        },
-        {
-          "cuisine": {
-            "cuisine_id": 193,
-            "cuisine_name": "BBQ"
-          }
-        },
-        {
-          "cuisine": {
-            "cuisine_id": 5,
-            "cuisine_name": "Bakery"
-          }
-        },
-        {
-          "cuisine": {
-            "cuisine_id": 227,
-            "cuisine_name": "Bar Food"
-          }
-        },
-        {
-          "cuisine": {
-            "cuisine_id": 270,
-            "cuisine_name": "Beverages"
-          }
-        },
+        }       
     ]
-
-// ******* create sample data for restaurant list return - id - name - address
-
-// ******* change the element type on the
-/* <article> 
-    
-</article> */
-// cuisinesArray = [
-//   {
-//     "cuisine": {
-//       "cuisine_id": 152,
-//       "cuisine_name": "African"
-//     }
-//   },
-/* <article id="cuisine-article">
-  <input type="text" name="Cuisines" id="cuisine-input" placeholder="Search by Cuisine">
-  <button type="submit" id="cuisine-search">Search RESTAURANTS</button>
-</article> */
-
-// const eventListeners = {
-//   handleSearchButton() {
-//     let searchInput = document.querySelector("input[name='searchinput']");
-//     let searchQuery = searchInput.value;
-
-//     data.queryResources(searchQuery)
-//     .then(searchResultsArray => {
-//       console.log(searchResultsArray);
-//       let resourcesFragments = domComponents.createResourcesDocumentFragments(searchResultsArray);
-//       console.log(resourcesFragments);
-//       domBuilder.appendAllResources(resourcesFragments);
-//     });
-//   }
-// };
 
 let cuisineSearchBtn = document.querySelector("#cuisine-search");
 cuisineSearchBtn.addEventListener("click", () => {
@@ -111,31 +42,3 @@ console.log("goodbye")
 //   input:invalid {
 //   border: 2px dashed red;
 // }
-
-
-// const eventListeners = {
-//     handleSearchButton() {
-//       let searchInput = document.querySelector("input[name='searchinput']");
-//       let searchQuery = searchInput.value;
-//       data.queryResources(searchQuery)
-//       .then(searchResultsArray => {
-//         console.log(searchResultsArray);
-//         let resourcesFragments = domComponents.createResourcesDocumentFragments(searchResultsArray);
-//         console.log(resourcesFragments);
-//         domBuilder.appendAllResources(resourcesFragments);
-//       });
-//     }
-//   };
-// var inventory = [
-//     {name: 'apples',
-//      quantity: 2},
-//     {name: 'bananas', quantity: 0},
-//     {name: 'cherries', quantity: 5}
-// ];
-
-// function isCherries(fruit) {
-  //     return fruit.name === 'cherries';
-// }
-
-// console.log(inventory.find(isCherries));
-// { name: 'cherries', quantity: 5 }
