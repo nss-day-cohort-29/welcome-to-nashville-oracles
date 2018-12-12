f2d8aca1a1e9e44f2bc815ba2d7060898102b9e6
Colleen Woolsey <rlwool@aol.com>
2018-12-11 22:36:29 -0600

    added const domComponents function in js

--- src/scripts/domComponents.js
+++ src/scripts/domComponents.js
@@ -1,1 +1,12 @@
 console.log("hello domComponents.js")
+
+const domComponents = {
+    createDomElement (elementType, content, cssClass) {
+      const element = document.createElement(elementType);
+      element.textContent = content;
+      if(cssClass) {
+        element.classList.add(cssClass);
+      }
+      return element;
+    }
+  }
